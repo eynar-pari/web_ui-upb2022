a3f72b518b25d5f5129eb827886b5929477df5ec
eynar-pari <eynar_repc@hotmail.com>
2022-03-08 08:09:50 -0400

    init

---
+++ src/test/java/pages/MainPage.java
@@ -0,0 +1,9 @@
+package pages;
+
+import control.Image;
+import org.openqa.selenium.By;
+
+public class MainPage {
+
+    public Image loginImage = new Image(By.xpath("//img[@src='/Images/design/pagelogin.png']"));
+}
---
+++ src/test/java/testClean/LoginTest.java
@@ -0,0 +1,24 @@
+package testClean;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.LoginModal;
+import pages.MainPage;
+import pages.MenuSection;
+
+public class LoginTest extends BaseTodoLy {
+    MainPage mainPage= new MainPage();
+    LoginModal loginModal= new LoginModal();
+    MenuSection menuSection = new MenuSection();
+
+    @Test
+    public void verifyLoginTodoLy(){
+        mainPage.loginImage.click();
+        loginModal.emailTxtBox.setText("clean@upb22.com");
+        loginModal.pwdTxtBox.setText("12345");
+        loginModal.loginButton.click();
+        Assertions.assertTrue(menuSection.logoutButton.isControlDisplayed(),
+                "ERROR no se pudo hacer el login");
+    }
+
+}
